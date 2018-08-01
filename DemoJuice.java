e631d7e6fec8691e21abda270a8c7f377acd5184
bezsden <42007706+bezsden@users.noreply.github.com>
2018-08-01 23:33:50 +0300

    Delete DemoJuice.java

--- DemoJuice.java
+++
@@ -1,7 +0,0 @@
-public class DemoJuice {
-    public static void main(String args[]){
-        FreshJuice juice = new FreshJuice();
-        juice.size = FreshJuice.FreshJuiceSize.MEDIUM;
-        System.out.println("Размер: " + juice.size);
-    }
-}
